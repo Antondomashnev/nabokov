70fb9bf74e3495536f81dc214a46cd34ada0c326
Anton Domashnev <antondomashnev@gmail.com>
2016-06-28 23:09:13 +0200

    Create a StringsFile model; Add Gemfile with rspec;

---
+++ lib/nabokov/core/strings_file_finder.rb
@@ -0,0 +1,12 @@
+require 'find'
+
+module Nabokov
+  class StringsFileFinder
+    attr_accessor :directory_path
+
+    def initialize(directory_path: nil)
+
+    end
+
+  end
+end
---
+++ lib/nabokov/models/strings_file.rb
@@ -0,0 +1,9 @@
+module Nabokov
+  class StringsFile
+
+    # @return [String] The extension of the strings file used by Xcode
+    def self.extension
+      return "strings"
+    end
+  end
+end
