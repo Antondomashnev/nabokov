7b34038cadcb20b4b56cc619004f720aa3c37251
Anton Domashnev <antondomashnev@gmail.com>
2016-07-12 22:38:42 +0200

    Create the skeleton of Runner;

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem "rspec", "~> 3.4"
-gem "git", "~> 1"
+gem "git", "~> 1.0"
+gem "claide", "~> 1.0"
---
+++ lib/nabokov/commands/runner.rb
@@ -0,0 +1,32 @@
+module Nabokov
+  class Runner < CLAide::Command
+
+    self.summary = 'Run the localization syncing.'
+    self.command = 'nabokov'
+    self.version = Nabokov::VERSION
+
+    def initialize(argv)
+      nabokovfile = argv.option('nabokovfile')
+      @nabokovfile_path = nabokovfile if File.exist? nabokovfile
+      super
+    end
+
+    def validate!
+      super
+      if self.class == Runner && !@nabokovfile_path
+        help! "Could not find a Nabokovfile."
+      end
+    end
+
+    def self.options
+      [
+        ['--nabokovfile=<path/to/nabokovfile>', 'The location of your Nabokovfile']
+      ].concat(super)
+    end
+
+    def run
+      
+    end
+
+  end
+end
