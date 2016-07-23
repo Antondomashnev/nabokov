521e0055c4adac7ac9a621f10f4fbdd8f6d1e25f
Anton Domashnev <antondomashnev@gmail.com>
2016-07-23 09:53:30 +0200

    Made pre_commit_file only readable for the world;

--- lib/nabokov/commands/setup.rb
+++ lib/nabokov/commands/setup.rb
@@ -9,11 +9,11 @@ module Nabokov
     self.command = 'setup'
     self.version = Nabokov::VERSION
 
-    attr_accessor :pre_commit_file
+    attr_reader :pre_commit_file
 
     def initialize(argv)
-      self.pre_commit_file = argv.option('pre_commit_file')
-      self.pre_commit_file ||= default_pre_commit_file
+      @pre_commit_file = argv.option('pre_commit_file')
+      @pre_commit_file ||= default_pre_commit_file
 
       @git_path = argv.option('git_path')
       @git_path ||= default_git_path
@@ -29,14 +29,18 @@ module Nabokov
     private
 
     def ensure_pre_commit_file_exists
-      self.pre_commit_file = File.realpath(self.pre_commit_file) if File.symlink?(self.pre_commit_file)
-      return if File.exists?(self.pre_commit_file)
+      @pre_commit_file = File.realpath(@pre_commit_file) if File.symlink?(@pre_commit_file)
+      return if File.exists?(@pre_commit_file)
 
       raise ".git folder is not found at '#{@git_path}'" unless Dir.exist?(@git_path)
 
       FileUtils::mkdir_p("#{@git_path}/hooks")
-      self.pre_commit_file = "#{@git_path}/hooks/pre-commit"
-      FileUtils.touch(self.pre_commit_file)
+      @pre_commit_file = "#{@git_path}/hooks/pre-commit"
+      FileUtils.touch(@pre_commit_file)
+    end
+
+    def ensure_pre_commit_file_is_executable
+      FileTest.executable?(@pre_commit_file)
     end
 
     def default_git_path
