945444da56a427c1b3e1f780eeb029d2ba4a7e02
Anton Domashnev <antondomashnev@gmail.com>
2016-07-23 18:10:31 +0200

    Setup command is completed;

--- spec/lib/nabokov/commands/setup_spec.rb
+++ spec/lib/nabokov/commands/setup_spec.rb
@@ -28,7 +28,7 @@ describe Nabokov::Setup do
         end
       end
 
-      it "creates a pre_commit_file at the given_git_path/hooks/pre_commit" do
+      it "creates a pre_commit_file at the given git_path/hooks/pre_commit" do
         setup = Nabokov::Setup.run(['--pre_commit_file=spec/fixtures/test_git_setup/missing_file', '--git_path=spec/fixtures/test_git_setup/git_folder/.git'])
         expect(File.exists?(setup.pre_commit_file)).to be_truthy
         FileUtils.rm_rf(Dir.glob("spec/fixtures/test_git_setup/git_folder/.git/*"))
@@ -39,6 +39,52 @@ describe Nabokov::Setup do
       it "raises an exception" do
         input = ['--pre_commit_file=spec/fixtures/test_git_setup/not_executable_pre_commit_file', '--git_path=spec/fixtures/test_git_setup/git_folder/.git']
         expect { Nabokov::Setup.run(input) }.to raise_error("pre commit file at 'spec/fixtures/test_git_setup/not_executable_pre_commit_file' is not executable by the effective user id of this process")
+      end
+    end
+
+    context "when current git repo is not listed in pre_commit_file" do
+      before(:each) do
+        input = ['--pre_commit_file=spec/fixtures/test_git_setup/existed_pre_commit_file', '--git_path=spec/fixtures/test_git_setup/git_folder/.git']
+        Nabokov::Setup.run(input)
+      end
+
+      after(:each) do
+        File.truncate('spec/fixtures/test_git_setup/existed_pre_commit_file', 0)
+      end
+
+      it "puts into pre commit line to tell that file is executable" do
+        result = File.foreach("spec/fixtures/test_git_setup/existed_pre_commit_file").any? { |line|
+          line.include?("#!\/usr\/bin\/env bash")
+        }
+        expect(result).to be_truthy
+      end
+
+      it "puts into pre commit line about current repo path" do
+        result = File.foreach("spec/fixtures/test_git_setup/existed_pre_commit_file").any? { |line|
+          line.include?("current_repo_path=\$(git rev-parse --show-toplevel)")
+        }
+        expect(result).to be_truthy
+      end
+
+      it "puts into pre commit line about nabokovfile path" do
+        result = File.foreach("spec/fixtures/test_git_setup/existed_pre_commit_file").any? { |line|
+          line.include?("nabokovfile_path=\"$current_repo_path\/Nabokovfile\"")
+        }
+        expect(result).to be_truthy
+      end
+
+      it "puts into pre commit line about tracking repo path" do
+        result = File.foreach("spec/fixtures/test_git_setup/existed_pre_commit_file").any? { |line|
+          line.include?("tracking_repo_path=")
+        }
+        expect(result).to be_truthy
+      end
+
+      it "puts into pre commit line to exectute nabokov" do
+        result = File.foreach("spec/fixtures/test_git_setup/existed_pre_commit_file").any? { |line|
+          line.include?("if [ \"$current_repo_path\" == \"$tracking_repo_path\" ] && gem list -i nabokov && [ -e \"$nabokovfile_path\" ]; then nabokov --nabokovfile=$nabokovfile_path || exit 1; fi")
+        }
+        expect(result).to be_truthy
       end
     end
   end
