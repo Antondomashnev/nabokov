44de99132236d52b1e67aa5ae7d3d76ce456cf35
Anton Domashnev <antondomashnev@gmail.com>
2016-08-07 14:36:05 +0200

    Set the nabokov bin command - repo sync command;

--- lib/nabokov.rb
+++ lib/nabokov.rb
@@ -2,6 +2,7 @@ require "nabokov/version"
 require "nabokov/core/nabokovfile"
 require "nabokov/commands/runner"
 require "nabokov/commands/syncers/syncer"
+require "nabokov/commands/syncers/repo_syncer"
 
 require "claide"
 require "pathname"
