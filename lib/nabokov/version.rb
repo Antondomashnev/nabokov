f0064ca326c7a3214ccd7c0103ca175c6f843d8b
Anton Domashnev <antondomashnev@gmail.com>
2016-09-10 13:42:47 +0200

    Bump version to 0.1.1;

--- lib/nabokov/version.rb
+++ lib/nabokov/version.rb
@@ -1,5 +1,5 @@
 module Nabokov
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
   DESCRIPTION = "One of the way to work on the localization - store it in the separate github repo
                  and asks localization team to update files in that repo.
                  This solution brings as well advantages but as well a disadvantages, to minimize the disadvantages
